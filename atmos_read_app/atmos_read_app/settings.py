c0019baa0b806b3882bad4a9707cc640f242a403
Abel-malusa21 <128817622+Abel-malusa21@users.noreply.github.com>
2024-08-22 15:32:11 +0200

    Update settings.py

--- atmos_read_app/atmos_read_app/settings.py
+++ atmos_read_app/atmos_read_app/settings.py
@@ -25,10 +25,7 @@ SECRET_KEY = 'django-insecure-rxqlwita_0(!a@^=vv3xwykx$hde2w@$o0hk%(@$-%&n-18u9r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '172.16.60.112.8020',
-    'localhost',
-    '127.0.0.1',
+ALLOWED_HOSTS = ['*',
 ]
 
 
