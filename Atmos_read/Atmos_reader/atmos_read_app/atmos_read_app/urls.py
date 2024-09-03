2a4e79130ca2c01290fa90eebf12acc06b858c2f
Abel-malusa21 <128817622+Abel-malusa21@users.noreply.github.com>
2024-09-03 13:04:26 +0200

    Delete Atmos_read/Atmos_reader directory

--- Atmos_read/Atmos_reader/atmos_read_app/atmos_read_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from monitor.views import index
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('monitor.urls')),  # Include API URLs from monitor app
-    path('', index, name='index'),  # Map the root URL to the index view
-]
