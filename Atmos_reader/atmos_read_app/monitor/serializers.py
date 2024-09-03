5d4e9829ee73b8536e560b21f36729cc00acb6c5
Abel-malusa21 <128817622+Abel-malusa21@users.noreply.github.com>
2024-09-03 13:46:50 +0200

    Delete Atmos_reader/atmos_read_app directory

--- Atmos_reader/atmos_read_app/monitor/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import TempHumidityRecord
-
-class TempHumidityRecordSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TempHumidityRecord
-        fields = ['id', 'temperature', 'humidity', 'timestamp']
--- Atmos_reader/atmos_read_app/monitor/urls.py
+++
@@ -1,8 +0,0 @@
-# urls.py in monitor
-from django.urls import path
-from .api import TempHumidityRecordListCreate, TempHumidityRecordDetail
-
-urlpatterns = [
-    path('records/', TempHumidityRecordListCreate.as_view(), name='record-list-create'),
-    path('records/<int:pk>/', TempHumidityRecordDetail.as_view(), name='record-detail'),
-]
